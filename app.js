e8cbb56adb77c2a3db388a36e50e171558359f50
Ritesh kaul <93066663+Riteshkaul@users.noreply.github.com>
2024-01-06 15:08:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ require("dotenv").config();
 require("./connection/conn.js");
 
 const bookRoute = require("./routers/bookRouter.js");
-app.use(cors());
+app.use(cors({
+              origint:["https://book-stack-server.vercel.app"],
+              methods:["POST","GET","PUT","DELETE"],
+              credentials: true,
+}
+            ));
 app.use(express.json());
 app.use("/book", bookRoute);
 app.get("/", (req, res) => {
