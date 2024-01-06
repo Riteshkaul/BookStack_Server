24b6e3caccfb773e203f13d239cdf2936f0db034
Ritesh kaul <93066663+Riteshkaul@users.noreply.github.com>
2024-01-06 15:52:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ const cors = require("cors");
 require("./connection/conn.js");
 
 const bookRoute = require("./routers/bookRouter.js");
-app.use(cors({
-              origin:["http://localhost:3000"],
-              methods:["POST","GET","PUT","DELETE"],
-              credentials: true,
-}
-            ));
+app.use(cors());
 app.use(express.json());
 app.use("/book", bookRoute);
 app.get("/", (req, res) => {
